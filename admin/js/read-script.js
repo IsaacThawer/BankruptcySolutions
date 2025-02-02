30f78484b8f4bea6cd1f385f0f014ae448cea75d
RobbyS3 <turgid-steep.0f@icloud.com>
2025-02-01 21:21:25 -0800

    SCRUM 135: Admin page able to modify Reviews page
    The Admin page is able to update or modify text information displayed on the Reviews page.

--- admin/js/read-script.js
+++ admin/js/read-script.js
@@ -4,7 +4,7 @@ async function loadText(contID, filename) {
         method: 'GET',
     }); // load from server
     const text = await response.text();
-    document.getElementById(contID).textContent = text;
+    document.getElementById(contID).innerHTML = text;
 }
 
 
@@ -14,3 +14,7 @@ window.addEventListener('DOMContentLoaded', loadText('home-page-contactInfo', 'i
 window.addEventListener('DOMContentLoaded', loadText('home-page-services', 'index-services.txt'));
 window.addEventListener('DOMContentLoaded', loadText('home-page-reviews', 'index-reviews.txt'));
 
+window.addEventListener('DOMContentLoaded', loadText('reviews-header', 'reviews-header.txt'));
+window.addEventListener('DOMContentLoaded', loadText('review1', 'review1.txt'));
+window.addEventListener('DOMContentLoaded', loadText('review2', 'review2.txt'));
+window.addEventListener('DOMContentLoaded', loadText('review3', 'review3.txt'));
